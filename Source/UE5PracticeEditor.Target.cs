e19ffa5d69f50d2038b4f25285f7c6df94b3500a
sam.chang <sam.chang@dji.com>
2023-05-18 17:37:43 +0800

    init

---
+++ Source/UE5PracticeEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class UE5PracticeEditorTarget : TargetRules
+{
+	public UE5PracticeEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		ExtraModuleNames.Add("UE5Practice");
+	}
+}
